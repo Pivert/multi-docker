c3519573067a409d7a9d862a0fe30733d57afcb4
Francois Delpierre <job@pivert.org>
2020-02-17 20:03:22 +0000

    Removed Logo

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,6 @@ class App extends Component {
       <Router>
           <div className="App">
             <div className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
               <h2>Welcome to React</h2>
               <Link to="/">Home</Link>
               <Link to="/otherpage">Other Page</Link>
